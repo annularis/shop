2e830a06058137b6362208264475ee01a05deed0
Dennis <danfossi@users.noreply.github.com>
2017-12-03 17:11:43 +0100

    Update main installer
    * Added CentOS support

--- install.sh
+++ install.sh
@@ -1,4 +1,7 @@
 if [ -f /etc/debian_version ]; then
     echo "Running Debian Setup.."
     ./installer/debian.sh
+elif [ -f /etc/centos-release ]; then
+    echo "Running CentOS Setup.."
+    ./installer/centos.sh
 fi
